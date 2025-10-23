14b84885da1b77407c73a137ccc7f72835da0350
Rachael Momber <109778611+RLMP44@users.noreply.github.com>
2025-10-23 17:53:08 +0000

    add: create component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDom from "react-dom/client";
 
 const root = ReactDom.createRoot(document.getElementById("root"));
+
+// react names are in pascal case (first letter capitalized)
+function Heading() {
+  return <h1>Favorite Foods</h1>
+}
+
+// if there are no children elements, best practice is to have self-closing tags
+// i.e. <Heading></Heading> becomes the below
 const html = (
   <div>
-    <h1>Favorite Foods</h1>
+    <Heading /> 
     <ul>
       <li>Sushi</li>
       <li>Katsudon</li>
